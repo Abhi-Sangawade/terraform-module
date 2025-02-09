56767d37dfcce2bca9393f2b4e24fb12fccb8805
Abhis-Sangawade <abhisheksangawade@gmail.com>
2025-02-09 22:02:20 +0530

    terraform module

--- vpc/main.tf
+++ vpc/main.tf
@@ -2,8 +2,6 @@
 
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
-  enable_dns_support = true
-  enable_dns_hostnames = true
 
   tags = {
     Name = "MainVPC"
@@ -13,7 +11,7 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "main" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.subnet_cidr
-  availability_zone       = "us-west-2a"
+  availability_zone       = "us-east-2a"
   map_public_ip_on_launch = true
 
   tags = {
@@ -41,4 +39,3 @@ resource "aws_security_group" "http_sg" {
   }
 }
 
-  
