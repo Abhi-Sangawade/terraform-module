02eeb194749a316d258f6f9f3a16a5d947be02c4
Abhis-Sangawade <abhisheksangawade@gmail.com>
2025-02-10 23:52:25 +0530

    Add files bucket

--- S3-host/main.tf
+++ S3-host/main.tf
@@ -1,23 +1,31 @@
 provider "aws" {
-  region = "ap-south-1"  
+  region = "ap-south-1"  # You can change the region if needed
 }
 
+# Declare the aws_region variable (if not using default)
+variable "aws_region" {
+  description = "The AWS region where resources will be created."
+  default     = "ap-south-1"  # You can modify this region if needed
+}
+
+# Create an S3 bucket
 resource "aws_s3_bucket" "static_website" {
-  bucket = "trfm-bucket"  
+  bucket = "trfm-bucket"  # Replace with a unique name
+}
 
+# Configure the S3 bucket as a static website
+resource "aws_s3_bucket_website_configuration" "static_website" {
+  bucket = aws_s3_bucket.static_website.bucket
 
-  website {
-    index_document = "index.html"
-  
+  index_document {
+    suffix = "index.html"
   }
 }
-
-
 resource "aws_s3_object" "website_index" {
   bucket = aws_s3_bucket.static_website.bucket
   key    = "index.html"
   source = "index.html"  
-  acl    = "public-read"  
+  acl    = "public-read" 
 }
 
 output "website_url" {
