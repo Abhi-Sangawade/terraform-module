e7509e87669c6fb08af9dcc0e8327099cc564a2c
Abhis-Sangawade <abhisheksangawade@gmail.com>
2025-02-10 23:49:45 +0530

    Add files bucket

--- S3-host/main.tf
+++ S3-host/main.tf
@@ -1,41 +1,25 @@
 provider "aws" {
-  region = "ap-south-1"
+  region = "ap-south-1"  
 }
 
 resource "aws_s3_bucket" "static_website" {
-  bucket = "trfm-bucket"
+  bucket = "trfm-bucket"  
+
+
+  website {
+    index_document = "index.html"
+  
+  }
 }
 
-resource "aws_s3_bucket_website_configuration" "static_website_config" {
+
+resource "aws_s3_object" "website_index" {
   bucket = aws_s3_bucket.static_website.bucket
-
-  index_document {
-    suffix = "index.html"
-  }
+  key    = "index.html"
+  source = "index.html"  
+  acl    = "public-read"  
 }
 
-resource "null_resource" "download_and_unzip" {
-  provisioner "local-exec" {
-    command = <<EOT
-      curl -O https://www.free-css.com/assets/files/free-css-templates/download/page296/listrace.zip -O /tmp/listrace.zip
-      unzip /tmp/listrace.zip -d /tmp/listrace/
-    EOT
-  }
-
-  triggers = {
-    always_run = "${timestamp()}"
-  }
-}
-
-resource "aws_s3_object" "static_files" {
-  for_each = fileset("/tmp/listrace", "*")
-
-  bucket = aws_s3_bucket.static_website.bucket
-  key    = each.value
-  source = "/tmp/listrace/${each.value}"
-  acl    = "public-read"
-}
 output "website_url" {
-  value = "http://${aws_s3_bucket.static_website.bucket}.s3-website-us-east-1.amazonaws.com" 
+  value = "http://${aws_s3_bucket.static_website.bucket}.s3-website-${var.aws_region}.amazonaws.com"
 }
-
